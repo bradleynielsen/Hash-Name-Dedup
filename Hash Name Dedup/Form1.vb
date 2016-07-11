fb2b0b4a7e2fe0a1754746ecaba09d6d33e71183
brad.nielsen <brad.nielsen@HPPC147>
2016-07-11 18:25:25 -0500

    stage 1

--- Hash Name Dedup/Form1.vb
+++ Hash Name Dedup/Form1.vb
@@ -1,3 +1,20 @@
-﻿Public Class Form1
+﻿Imports System.IO
 
+Public Class Form1
+    Dim userSelectedPath As String
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+    '
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim Dialog As FolderBrowserDialog = New FolderBrowserDialog
+        Dim folderDirectory As String
+        folderDirectory = Space$(255)
+        Dialog.Description = "Choose a directory..."
+        Dialog.ShowDialog(Me)
+        folderDirectory = Dialog.SelectedPath
+        RTrim(folderDirectory)
+        userSelectedPath = folderDirectory
+    End Sub
 End Class
